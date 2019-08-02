9834f5ccc872c5fb86d9ada119b514ef738521ad
gregory.gao <gregory.gao@TRAINING013.optimum.net>
2019-08-02 11:42:03 +0800

    11:41 2/8

--- jdbc/jdbc/Database01.java
+++ jdbc/jdbc/Database01.java
@@ -1,6 +1,7 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,14 +16,44 @@ public class Database01 {
 		System.out.println("Creating table in given database...");
 		Statement st = con.createStatement();
 		
-		//create table
-		String sql = "CREATE TABLE students " +
-		"(mobile INTEGER not NULL, " +
-				" name VARCHAR(25), " +
-		" email varchar(35), " + 
-				" PRIMARY KEY (mobile))";
-		st.executeUpdate(sql); //for saving purpose
-		System.out.println("Createedtable in given database...");
+//		//create table
+//		String sql = "CREATE TABLE students3 " +
+//		"(mobile INTEGER not NULL, " +
+//				" name VARCHAR(25), " +
+//		" email varchar(35), " + 
+//				" PRIMARY KEY (mobile))";
+//		st.executeUpdate(sql); //for saving purpose
+//		System.out.println("Created table in given database...");
+		
+//		//insert record
+//		String sql = "INSERT INTO students " + 
+//		"VALUES (123, 'john', 'john@gmail.com')";
+//		st.executeUpdate(sql);
+//		
+//		System.out.println("Data Inserted Successfully..");
+		
+//		//edit a record
+//		String sql = "UPDATE students " + "SET name = 'johnny' WHERE mobile in (123)";
+//		st.execute(sql);
+//		System.out.println("Record Modified Successfully");
+//	
+//		delete a record 
+//		String sql = "DELETE FROM students " +
+		
+//		//retrieve data from table
+		String sql = "SELECT mobile, name, email FROM students";
+		ResultSet rs = st.executeQuery(sql); //if you wanna print something.
+		
+//		
+		// Extract data from result set
+		System.out.println("\n\n");
+		System.out.println("Mobile\t\t" + "Name" + "\t\t" + "Email");
+		System.out.println("=================================================================");
+		
+		while (rs.next()){
+			System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t\t" + rs.getString(3));
+		}
+		
 	}
 
 }
---
+++ newproject/pojo/Person.java
@@ -0,0 +1,38 @@
+package pojo;
+
+public class Person {
+	private int id;
+	private String name;
+	private String password;
+	private String dob;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getDob() {
+		return dob;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+}
